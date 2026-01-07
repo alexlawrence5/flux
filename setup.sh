0f1a6b078125d9ef3dc3cb10e926a4e800f1fb05
Nuri <nurisworkspace00.com>
2026-01-07 14:27:37 +0100

    Version 2.0 beta

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -S figlet
+figlet "Flux"
+echo "Flux version: 2.0"
+sleep 10
+
+echo "Setting up Hyprpaper... (1920x1080 recommended)"
+mkdir $HOME/.fluxstuff
+wget -O "$HOME/.fluxstuff/wall.jpg" \
+https://wallpapers.com/images/featured-full/ultra-hd-wazf67lzyh5q7k32.jpg
+cp hyprpaper.conf ~/.config/hypr/
+
+echo "Setting up config files..."
+cp hyprland.conf ~/.config/hypr/
+
+echo "Setting up Waybar..."
+rm -rf $HOME/.config/waybar
+cp -r  waybar $HOME/.config/
